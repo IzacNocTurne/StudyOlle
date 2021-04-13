355f72958c889434c6e9cc0b001d81eea42a2216
IzacNocTurne <sonth2000@gmail.com>
2021-04-13 22:54:15 +0900

    구현한 로직 가입 확인 이메일을 재전송할 수 있는 기능 제공 1시간에 한번만 인증 메일을 전송할 수 있도록 제한한다.
    GET “/check-email”
    가입 확인 이메일을 전송한 이메일 주소 (== 가입할 때 입력한 이메일 주소)를 화면에 보여줌.
    재전송 버튼 보여주기.
    재전송 버튼 클릭하면 GET “/resend-confirm-email” 요청 전송
    
    GET “/resend-confirm-email”
    인증 메일을 다시 전송할 수 있는지 확인한 뒤에
    보낼 수 있으면 전송하고, 첫 페이지로 리다이렉트
    보낼 수 없으면 에러 메시지를 모델에 담아주고 이메일 확인 페이지 다시 보여주기.

--- src/main/java/com/studyolle/account/AccountController.java
+++ src/main/java/com/studyolle/account/AccountController.java
@@ -1,6 +1,7 @@
 package com.studyolle.account;
 
 import com.studyolle.domain.Account;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -65,7 +66,24 @@ public class AccountController {
         model.addAttribute("numberOfUser", accountRepository.count());
         model.addAttribute("nickname", account.getNickname());
         return view;
+    }
 
+    @GetMapping("/check-email")
+    public String checkEmail(@CurrentUser Account account, Model model) {
+        model.addAttribute("email", account.getEmail());
+        return "account/check-email";
+    }
+
+    @GetMapping("/resend-confirm-email")
+    public String resendConfirmEmail(@CurrentUser Account account, Model model) {
+        if (!account.canSendConfirmEmail()) {
+            model.addAttribute("error", "인증 이메일은 1시간에 한번만 전송할 수 있습니다.");
+            model.addAttribute("email", account.getEmail());
+            return "account/check-email";
+        }
+
+        accountService.sendSignUpConfirmEmail(account);
+        return "redirect:/";
     }
 
 
